4f02062169e3bcd6bd29cac1aeba32fc29727fbe
merQ224 <wnwinstonn95@gmail.com>
2025-07-21 14:16:33 +1200

    home page component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./pages/home-page/home-page.route').then((m) => m.routes),
+  },
+  {
     path: 'dropdown',
     loadChildren: () => import('./pages/dropdown-hub/dropdown-hub.route').then((m) => m.routes),
   },
   {
     path: '',
-    redirectTo: 'dropdown',
+    redirectTo: 'home',
     pathMatch: 'full'
   }
 ];
